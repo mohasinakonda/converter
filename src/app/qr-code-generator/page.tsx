38998fcc24a31ee0a878c3a67bf3d057d828c498
mohasinakonda <mohasinakonda39@gmail.com>
2025-06-15 20:27:55 +0600

    chore: add meta description and keyword

--- src/app/qr-code-generator/page.tsx
+++ src/app/qr-code-generator/page.tsx
@@ -5,6 +5,15 @@ import QRCode from "qrcode";
 import Link from "next/link";
 import { ArrowsRightLeftIcon } from "@heroicons/react/24/outline";
 import { Input } from "@/components/ui/input";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+	title: "QR Code Generator",
+	description:
+		"Generate QR codes from any text and download them instantly as high-quality PDF files. Fast, free, and easy-to-use QR Code Generator app.",
+	keywords:
+		"QR code generator, text to QR, download PDF QR code, free QR code app, QR code maker, text QR code, QR code PDF download, instant QR code, customizable QR codes, QR code online,best qr code generator online,qr code generator,qr code generator app,qr code generator online,qr code generator tool,qr code generator website,qr code generator tool online,qr code generator website online,qr code generator tool for free,qr code generator website for free,qr code generator tool for business,qr code generator website for business,qr code generator tool for personal use,qr code generator",
+};
 const QRCodeGenerator = () => {
 	const [inputText, setInputText] = useState("");
 	const [qrCodeDataUrl, setQrCodeDataUrl] = useState("");
--- src/app/qr-code-reader/page.tsx
+++ src/app/qr-code-reader/page.tsx
@@ -1,9 +1,16 @@
 import QrCodeReader from "@/components/QrCodeReader";
-
+import { Metadata } from "next";
+export const metadata: Metadata = {
+	title: "QR Code Reader",
+	description:
+		"Scan and decode QR codes instantly with our fast, free QR Code Reader app.",
+	keywords:
+		"qr code scanner, qr code reader, code scanner app, scan qr code fast, free qr scanner, qr code reader tool, qr code scan app, qr reader for wifi, qr code decoder, qr code reader online,",
+};
 export default function QrCodeReaderPage() {
-  return (
-    <div className="min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] flex flex-col items-center">
-      <QrCodeReader />
-    </div>
-  );
+	return (
+		<div className="min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] flex flex-col items-center">
+			<QrCodeReader />
+		</div>
+	);
 }
